09a3974a9a09c1b910eff854e860648f6dd03f2e
ShaviyaVictor <shaviyavictor@gmail.com>
2022-02-27 16:54:46 +0300

    Add category model class

--- memoir/models.py
+++ memoir/models.py
@@ -1,3 +1,12 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class categories(models.Model) :
+  name = models.CharField(max_length=30)
+
+  def __str__(self) -> str:
+      return self.name
+
+  class Meta :
+    ordering = ['-name']
