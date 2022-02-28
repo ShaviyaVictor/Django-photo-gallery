cf737af476b906d95c75e7e98d04b96f6591adc7
ShaviyaVictor <shaviyavictor@gmail.com>
2022-02-28 05:52:50 +0300

    Run test for category instance

--- memoir/tests.py
+++ memoir/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import categories, location, photos
 
 # Create your tests here.
+class categoriesTestClass(TestCase) :
+  # Set up method
+  def setUp(self) :
+    self.top = categories(id='1')
+
+  # Testing the instance
+  def test_instance(self) :
+    self.assertTrue(isinstance(self.top, categories))
